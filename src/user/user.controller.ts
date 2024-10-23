6f8e5388f58a30f3760a5512c2fb794493dd6058
sudanmd <sudanmd2002@gmail.com>
2024-10-23 15:29:47 +0530

    adding user controller

---
+++ src/user/user.controller.ts
@@ -0,0 +1,41 @@
+import { 
+  Controller, 
+  Get, Param, Post, Body, Put, Delete} from '@nestjs/common';
+import { UserService } from './user.service';
+import { Userdto, updateuserDto } from './user.dto';
+
+
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usermodel: UserService) {}
+
+  @Post()
+  async createUser(@Body() body : Userdto ){
+    return await this.usermodel.createUser(body);
+  }
+
+  // Get all users (GET)
+  @Get()
+  async getAllUsers() {
+    return await this.usermodel.getAllUser()
+  }
+
+  // Get a user by ID (GET)
+  @Get(':Id')
+  async getone(@Param('Id') userId: number) {
+  return await this.usermodel.getOne(userId);
+  }
+
+  @Put(':userId')
+  async update(@Param('userId') userId: number, @Body()users:updateuserDto) {
+    return await this.usermodel.updateuser(userId,users);
+  }
+
+  // Delete a user by ID (DELETE)
+  @Delete(':userId')
+  async deleteUser(@Param('userId') id: number) {
+   await this.usermodel.deleteUser(id);
+   
+  }
+}
